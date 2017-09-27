4837aee7ad7a90ba0001ee43fb7dd10abd4599ad
Daniel Frentzel <danielfrentzel@gmail.com>
2017-09-27 14:00:16 -0600

    Created P3 object and updated Flock
    P3 class added to hold points for position and velocity, along with
    their computations. Updated Flock update method and changed position
    and velocity to P3 objects.

--- src/Flock.py
+++ src/Flock.py
@@ -1,27 +1,31 @@
 import Boid
+import P3
 
 
 class Flock:
-    flockcount = 0
+    flock_count = 0
 
     def __init__(self, num_boids, center, radius): #possibly also orientation, maybe something about obstacles?
         self.num_boids = num_boids
         self.center = center
         self.radius = radius
-        self.boid_list = []
-        self.flockcount += 1
+        self.boids = []
+        self.flock_count += 1
         self.distance_matrix = [[0 for _ in range(num_boids)] for _ in range(num_boids)]
 
+        # defaults boid to grid
         row = -1
         for i in range(num_boids):
             if i % 10 == 0:
                 row += 1
-            self.boid_list.append(Boid(i, (200 - 10*row, -20 + 4*i, 50), (-22, 0, 0), 0)) #if velocity and position are tuples
+            self.boids.append(Boid(self, i, P3(200 + 10 * row, -20 + 4 * i, 50), P3(-20, 0, 0)))
 
-    def update(self):
-        return self.boid_list
+    def update(self, tick):
+        for b in self.boids:
+            b.self.move(tick)  # add other things the Boid.move() needs
+        self.dist_matrix()
 
     def dist_matrix(self):
-        for i in range(len(self.boid_list)):
-            for j in range(len(self.boid_list)-i):
-                self.distance_matrix[i][j] = Boid.distance(self.boid_list[i], self.boid_list[j]) #using Boid.distance method
+        for i in range(len(self.boids)):
+            for j in range(len(self.boids)-i):
+                self.distance_matrix[i][j] = self.boids[i].position.distance(self.boids[j].position)
