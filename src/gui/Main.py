654f84d6e55cbc41619cef4d6f6c19e2d1c05f56
Isaac Casteel-Denney <midnightgarden101@gmail.com>
2017-09-27 11:50:37 -0600

    Updated GUI

--- src/gui/Main.py
+++ src/gui/Main.py
@@ -40,36 +40,42 @@ class GUI(tk.Frame):
 
         self.init_flock_frame()
 
-
-
-
-        #frame3 = tk.Frame(self)
-        #frame3.pack(side=tk.LEFT, anchor=tk.N)
-
-        #label_flock_position = tk.Label(frame3, text="Flock Position")
-        #label_flock_position.pack(side=tk.LEFT, padx=5, pady=5)
-
-        #entry_flock_position_x = tk.Entry(frame3)
-        #entry_flock_position_x.pack(side=tk.LEFT,padx=5, pady=5)
-
-        #entry_flock_position_y = tk.Entry(frame3)
-        #entry_flock_position_y.pack(side=tk.LEFT,padx=5, pady=5)
-
     def init_flock_frame(self):
+
+
         frame_flock = tk.Frame(self, bd=2, relief=tk.GROOVE)
         frame_flock.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
 
-        label_flock_settings = tk.Label(frame_flock, text="Flock Settings")
+        label_flock_settings = tk.Label(frame_flock, text="Flocks")
         label_flock_settings.pack(side=tk.TOP, padx=5, pady=5)
 
-        frame_flock_count = tk.Frame(frame_flock, bd=1, relief=tk.RAISED)
-        frame_flock_count.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
+        frame_flock_list = tk.Frame(frame_flock, bd=1, relief=tk.RAISED)
+        frame_flock_list.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
 
-        label_flock_count = tk.Label(frame_flock_count, text="Count")
-        label_flock_count.pack(side=tk.TOP, padx=5, pady=5)
+        frame_flock_list_1 = tk.Frame(frame_flock_list)
+        frame_flock_list_1.pack(side=tk.LEFT,padx=1)
 
-        text_flock_count = tk.Text(frame_flock_count, height=1, width=2)
-        text_flock_count.pack(side=tk.TOP, padx=5, pady=5)
+        label_flock = tk.Label(frame_flock_list_1, text="Flock")
+        label_flock.pack(side=tk.TOP, padx=5, pady=5)
+
+        listbox_flocks_scrollbar = tk.Scrollbar(frame_flock_list_1,orient=tk.VERTICAL)
+        listbox_flocks = tk.Listbox(frame_flock_list_1,yscrollcommand=listbox_flocks_scrollbar.set,name='listbox_flocks',height=1,width=16)
+        #listbox_flocks_scrollbar.config(command=listbox_flocks_scrollbar.yview)
+        listbox_flocks_scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
+
+        listbox_flocks.pack(side=tk.TOP,fill=tk.Y)
+        listbox_flocks.insert(tk.END, "0")
+        listbox_flocks.insert(tk.END, "1")
+        listbox_flocks.bind('<<ListboxSelect>>', GUI.on_select)
+
+        frame_flock_list_2 = tk.Frame(frame_flock_list)
+        frame_flock_list_2.pack(side=tk.LEFT,padx=1,fill=tk.BOTH)
+
+        button_remove_flock = tk.Button(frame_flock_list_2,text="Remove Flock")
+        button_remove_flock.pack(side=tk.BOTTOM,fill=tk.BOTH)
+
+        button_add_flock = tk.Button(frame_flock_list_2, text="Add Flock")
+        button_add_flock.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
         frame_flock_size = tk.Frame(frame_flock, bd=1, relief=tk.RAISED)
         frame_flock_size.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
@@ -80,25 +86,25 @@ class GUI(tk.Frame):
         text_flock_size = tk.Text(frame_flock_size, height=1, width=2)
         text_flock_size.pack(side=tk.TOP, padx=5, pady=5)
 
-        frame_flock_default_position = tk.Frame(frame_flock, bd=1, relief=tk.RAISED)
-        frame_flock_default_position.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
+        frame_flock_position = tk.Frame(frame_flock, bd=1, relief=tk.RAISED)
+        frame_flock_position.pack(side=tk.LEFT, anchor=tk.N, padx=5, pady=5)
 
-        label_flock_default_position = tk.Label(frame_flock_default_position, text="Default Position")
-        label_flock_default_position.pack(side=tk.TOP, padx=5, pady=5)
+        label_flock_position = tk.Label(frame_flock_position, text="Position")
+        label_flock_position.pack(side=tk.TOP, padx=5, pady=5)
 
-        frame_flock_default_position_1 = tk.Frame(frame_flock_default_position)
-        frame_flock_default_position_1.pack(side=tk.TOP)
+        frame_flock_position_1 = tk.Frame(frame_flock_position)
+        frame_flock_position_1.pack(side=tk.TOP)
 
-        label_flock_default_position_x = tk.Label(frame_flock_default_position_1, text="X: ")
+        label_flock_default_position_x = tk.Label(frame_flock_position_1, text="X: ")
         label_flock_default_position_x.pack(side=tk.LEFT, padx=5, pady=5)
 
-        text_flock_default_position_x = tk.Text(frame_flock_default_position_1, height=1, width=4)
+        text_flock_default_position_x = tk.Text(frame_flock_position_1, height=1, width=4)
         text_flock_default_position_x.pack(side=tk.LEFT, padx=5, pady=5)
 
-        label_flock_default_position_y = tk.Label(frame_flock_default_position_1, text="Y: ")
+        label_flock_default_position_y = tk.Label(frame_flock_position_1, text="Y: ")
         label_flock_default_position_y.pack(side=tk.LEFT, padx=5, pady=5)
 
-        text_flock_default_position_y = tk.Text(frame_flock_default_position_1, height=1, width=4)
+        text_flock_default_position_y = tk.Text(frame_flock_position_1, height=1, width=4)
         text_flock_default_position_y.pack(side=tk.LEFT, padx=5, pady=5)
 
         return frame_flock
@@ -110,6 +116,12 @@ class GUI(tk.Frame):
     #    app = GUI(root)
 
     #    root.mainloop()
+
+    def on_select(evt):
+        w=evt.widget
+        index=int(w.curselection()[0])
+        value=w.get(index)
+        r = value
 
 if __name__=='__main__':
     root = tk.Tk()
