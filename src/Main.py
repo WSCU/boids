e98e7b9cef4fdc50e692bf338cb1964e10f5fbe8
admin <joshua.fry@western.edu>
2017-09-29 13:19:05 -0600

    trying out fork

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,4 @@
-
+#world_01
 import WorldObjects
 from World import World
 import threading
@@ -9,6 +9,7 @@ z = 40
 buildings = {}
 
 world = World(guiX,guiY,z,buildings)
+f1 = Flock(5, )
 
 def printit():                           #this will be replaced with incremental function calls
   threading.Timer(1.0, printit).start()  #such as flock and boid directional and velocity changes
