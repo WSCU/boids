1d529d7d42523cd303b0009e8a9ca5712c8c68f2
Daniel Frentzel <danielfrentzel@gmail.com>
2017-10-02 15:35:25 -0600

    Change default z coordinate to 0

--- src/Flock.py
+++ src/Flock.py
@@ -6,7 +6,7 @@ import random
 
 class Flock:
     flock_count = 0
-    
+
     def __init__(self, num_boids, center=P3.P3(0, 0, 0), radius=20): #possibly also orientation, maybe something about obstacles?
         Flock.flock_count += 1
         self.boids = []
@@ -18,7 +18,7 @@ class Flock:
             theta = 2 * math.pi * random.random()
             x = r * math.cos(theta)
             y = r * math.sin(theta)
-            self.boids.append(Boid.Boid(self, i, P3.P3(x, y, 50) + center, P3.P3(-5, 0, 0), P3.P3(0, 0, 0)))
+            self.boids.append(Boid.Boid(self, i, P3.P3(x, y, 0) + center, P3.P3(-5, 0, 0), P3.P3(0, 0, 0)))
 
     def update(self, tick):
         for b in self.boids:
@@ -32,6 +32,6 @@ class Flock:
 
 
 if __name__ == '__main__':
-    flock = Flock(10, P3.P3(100, 100, 1), 1)
+    flock = Flock(10, P3.P3(100, 100, 50), 1)
     for b in flock.boids:
         print(b.position)
