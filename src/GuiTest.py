b266ee00462a6201745831630674e64bb8b87bc6
Isaac Casteel-Denney <midnightgarden101@gmail.com>
2017-10-03 22:41:50 -0600

    Updated GUI

---
+++ src/GuiTest.py
@@ -0,0 +1,22 @@
+import src.Gui
+
+class Application:
+
+    def __init__(self):
+        self.gui=src.Gui.GUI()
+        self.is_visible=True
+        self.gui.start(self.tick,1500)
+
+    def print_console(self):
+        print("Hello")
+
+    def tick(self):
+        self.print_console()
+        self.is_visible = not self.is_visible
+        self.gui.set_visible(self.is_visible)
+
+if __name__ == '__main__':
+    app = Application()
+
+    #gui.set_visible(False)
+    #gui.set_visible(True)
