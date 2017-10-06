085d3dcc486d0607358662d8abe79ab85d997136
Cody Bontecou <cody.bontecou@western.edu>
2017-10-06 11:05:12 -0600

    updated Main

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,9 @@ import WorldObjects
 from World import World
 import threading
 from src.Flock import Flock
+from src.render import Render
+import time
+import P3
 
 guiX = 40
 guiY = 40
@@ -10,21 +13,19 @@ z = 40
 c = 0
 r = 0
 buildings = {}
+tick = 0.05
 
 world = World(guiX,guiY,z,buildings)
 
-F1 = Flock(5, c, r)
+F1 = Flock(100, P3.P3(-20, 0, 0), 20)
+F1_r = Render(F1)
 
-def updateTick():
-  threading.Timer(0.05, F1.update).start()
-  print (world.timer.get_time_s())
+for i in range(7000):
+  print(tick)
+  F1_r.draw()
+  time.sleep(tick)
+
 
 #can change to pass update and tick to render
-F1.update(0.05)
+# F1.update(0.05)
 
-
-def printit():                           #this will be replaced with incremental function calls
-  threading.Timer(1.0, printit).start()  #such as flock and boid directional and velocity changes
-  print (world.timer.get_time_s())       #instead of displaying time every second
-
-printit()
