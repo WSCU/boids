44a37a22381b9725306e8dc757cf1de6d19f5219
StephColton <stephanie.colton@western.edu>
2017-10-06 12:09:06 -0600

    Updating World file's constructor so it works with the GUI.

--- src/World.py
+++ src/World.py
@@ -1,9 +1,8 @@
 from Timer import Timer
 class World:
 
-    def __init__ (self,x, y, z, buildings):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.buildings = buildings
-        self.timer = Timer()
+    def __init__ (self,screen, objects,flocks):
+        self.screen = screen
+        self.objects = objects
+        self.flocks = flocks
+
