8ace8fec6b7077738fd462180e991606a2d261c1
admin <joshua.fry@western.edu>
2017-10-10 13:32:03 -0600

    Turning World to Proper class

--- src/World.py
+++ src/World.py
@@ -13,40 +13,31 @@ class World:
         if isinstance(settings, config.Config_World):
             self.settings = settings
         self.gui = gui
-        render.start(self.settings.screen.x_size, self.settings.screen.y_size,1000)
 
-    def drawFlocks(self, flocks):
-        for b in flocks.boids:
-            b.move_Boid(tick)
+        if isinstance(settings.screen, config.Config_Screen):
+            render.start(self.settings.screen.x_size, self.settings.screen.y_size,1000)
 
+        F1 = Flock.Flock(100, P3.P3(-20, 0, 0), 20)
+        self.flock = render.Render(F1)
 
+        for num in range(10):
+            render.Buildings(render.random.randrange(-10, 10), render.random.randrange(-10, 10),
+                             render.random.randrange(1, 10), render.random.randrange(1, 10),
+                             render.random.randrange(1, 10), (
+                             render.random.randrange(0, 2), render.random.randrange(0, 2),
+                             render.random.randrange(0, 2)))
 
+    def drawFlocks(self):
+        for b in self.flock.boids:
+            b.move_Boid(self.tick)
 
-if __name__ == "__main__":
+    def tick(self):
+        self.drawFlocks(self.flock)
+        return True
 
-
+"""
     timer = Timer.Timer() #used for measuring elapsed time
 
-    tick = 0.05
-
-    F1 = Flock.Flock(100, P3.P3(-20, 0, 0), 20)
-
-    F1_r = render.Render(F1)
-
-    for num in range(10):
-        render.Buildings(render.random.randrange(-10, 10), render.random.randrange(-10, 10),render.random.randrange(1, 10), render.random.randrange(1, 10),
-                     render.random.randrange(1, 10), (render.random.randrange(0, 2), render.random.randrange(0, 2), render.random.randrange(0, 2)))
-
-
-
-    for i in range(7000):
-        F1_r.draw()
-        for b in F1.boids:
-            b.move_Boid(tick)
-
-        print(timer.get_time_s())
-        time.sleep(tick * timer.get_time_s()) #keeping even frame rate by multiplying tick by time elapsed during for loop iteration
-
-        timer.restart()
-
+    timer.restart()
+"""
 
