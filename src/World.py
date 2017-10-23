e0235ab6601282b01f599a1530ea4d0aff0032cd
Caleb J. Smith <Caleb.smith1149@gmail.com>
2017-10-23 13:29:17 -0600

    Update World
    Add make_force_field
    Move  flock init & make it a local variable

--- src/World.py
+++ src/World.py
@@ -9,7 +9,6 @@ import random
 
 buildings = []
 
-
 class World:
     tick_time = 10
     tick_step = 0.05
@@ -21,12 +20,8 @@ class World:
     def __init__(self, settings, gui):
         if isinstance(settings, Config.Config_World):
             self.settings = settings
-            flock = settings.flocks[0]
-            if isinstance(flock, Config.Config_Flock):
-                F1 = Flock.Flock(flock.count, flock.center, flock.radius)
+           
         self.gui = gui
-
-        self.render = render.Render(F1)
 
         if isinstance(settings.screen, Config.Config_Screen):
             render.start(self.settings.screen.x_pos, self.settings.screen.y_pos, self.settings.screen.x_size,
@@ -53,7 +48,13 @@ class World:
                         buildings.extend(
                             [Building.Building(b_location, 10, random.randrange(20, 65), 10, (self.c, 0, self.c2))])
                         self.x_location += 10
-
+                        
+        if isinstance(settings, Config.Config_World):
+            flock = settings.flocks[0]
+            if isinstance(flock, Config.Config_Flock):
+                self.F1 = Flock.Flock(flock.count, World.make_force_field(buildings), flock.center, flock.radius)
+        
+        self.render = render.Render(self.F1)
         gui.set_tick_method(self.tick)
 
     def drawFlocks(self):
@@ -72,3 +73,27 @@ class World:
         self.gui.set_tick_method(None)
         self.render.dispose()
         self.render = None
+        
+        def make_force_field(buildings):
+        buildingList = [] # holds touple of center and radius
+
+        for building in buildings:
+            buildingPos = P3.P3(building.x, building.y, building.z)
+            c = P3.P3(building.x + (building.width / 2), building.y + (building.height / 2),
+                      building.z + (building.depth / 2))             # Center of the building
+            buildingList.append((c, math.sqrt((building.width/2)**2 + (building.width/2)**2)))
+        def force_field(boidPosition):
+            nonlocal buildingList
+            sum = P3.P3(0, 0, 0)
+            for buildingPosition in buildingList:
+                c,rb = buildingPosition # Unpacking
+                r = boidPosition - c # Vector between the building center and the boidPosition
+                d = r.magnitude() # The magnitude of the vector between building center and boidPosition
+                fdir = 1/d * r # The direction of the force from building to boid
+                fmag = 1/(d - rb) # Magnitude of the force
+                force = fdir * fmag # Create final force to be returned from its vector
+                sum += force # Return the final force on the given boid position (P3)
+            return sum
+
+        return force_field
+
